bc94d9a2087660f42d6eb89fe23d0d3a589e338e
aaron <aaron.m.bray@gmail.com>
2016-04-20 16:37:41 -0700

    Added directions

--- labs/section2/RunTimeLinking/linux/main.cpp
+++ labs/section2/RunTimeLinking/linux/main.cpp
@@ -2,6 +2,11 @@
 #include <stdio.h>
 
 
+/**
+* The .so libloadable.so exports a single function:
+* int exported(int). You will need to load the library,
+* then find and call the export.
+*/
 int main(int argc, char** argv)
 {
 
