2e2b20167dec51518b72947226543e2911bd9358
aaron <aaron.m.bray@gmail.com>
2016-06-27 15:04:54 -0700

    Added another lab

--- labs/sources/lab2/main.c
+++ labs/sources/lab2/main.c
@@ -2,60 +2,25 @@
 #include <stdlib.h>
 #include <Windows.h>
 
-#define BUFFER_SIZE 512
-char buffer[BUFFER_SIZE];
-//char magicStr[] = "Hey dev, Without you my world is NULL.";
-char magicStr[] = "bOS\nNO\\\x06\n}C^BE_^\nSE_\nGS\n]EXFN\nCY\nd\x7f\x66\x66\x04"; //xor with 42
-#define XOR_KEY 42
 
-HANDLE openFile(LPCTSTR filename) {
-    return CreateFile(filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-}
 
-void deoffuscate(char* buffer, int size) {
-    int magicSize = 0;
-    int i;
-    if (magicSize > size) {
-        printf("Your heart is too small.\n");
-        return;
-    }
+int __cdecl main(int argc, char* argv[], char* envp[]) 
+{
+    char buf[MAX_PATH + 1] = {0};
+    DWORD value = 0;
+    char* tmp = NULL;
 
-    memset(buffer, 0, size);
+    if(argc < 3)
+        return -1;
 
-    magicSize = strlen(magicStr);
-    for (i = 0; i < magicSize; ++i) {
-        buffer[i] = magicStr[i] ^ 42;
-    }
-}
+    if(0 == (value = strtoul(argv[1], NULL, 0)))
+        return -2;
 
-int __cdecl main(int argc, char* argv[]) {
+    tmp = argv[2];
+    if(value != strlen(tmp))
+        return -3;
 
-    HANDLE hFile = openFile("C:\\badlove.dat");
-    DWORD bytesRead = 0;
-    DWORD err = 0;
-    char magic[BUFFER_SIZE] = { 0 };
-
-    memset(buffer, 0, BUFFER_SIZE);
-
-    if (hFile == INVALID_HANDLE_VALUE) {
-        err = GetLastError();
-        printf("My love is lost\n");
-        exit(0);
-    }
-
-    if (!ReadFile(hFile, buffer, BUFFER_SIZE, &bytesRead, NULL)) {
-        printf("I can't read your love\n");
-        exit(0);
-    }
-
-    deoffuscate(magic, BUFFER_SIZE);
-    if (strstr(buffer, magic)) {
-        printf("You have a const pointer to my microprocessor.\n");
-        printf("Congrats... your done!\n");
-    }
-    else {
-        printf("What is? %s\n", buffer);
-    }
+    MessageBoxA(NULL, "[*] Success!", "Success", MB_OK);
     
     return 0;
 }
